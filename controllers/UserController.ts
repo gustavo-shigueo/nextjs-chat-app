a857ba5d0998de216872e01b0f425b603d42e242
gustavo-shigueo <gustavo.gsmn@gmail.com>
2021-12-13 16:08:56 -0300

    Separating auth from user model

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,106 +1,57 @@
-import bcrypt from 'bcrypt'
-import UserModel from 'models/UserModel'
-import UserInterface from 'interfaces/UserInterface'
-import EmptyFields from 'validations/EmptyFields'
 import GoogleProfileInterface from 'interfaces/GoogleProfileInterface'
+import UserDocumentInterface from 'interfaces/UserDocumentInterface'
+import UserInterface from 'interfaces/UserInterface'
+import UserModel from 'models/UserModel'
 
 class UserController {
-	static async signupWithEmailAndPassword({
+	static findByEmail(email: string) {
+		return UserModel.findOne({ email })
+	}
+
+	static async checkEmail(email: string): Promise<boolean> {
+		return UserModel.exists({ email })
+	}
+
+	static async createFromEmailAndPassword({
 		name,
 		email,
 		password,
-		avatarUrl,
-	}: UserInterface) {
-		const [hasEmptyFields, emptyFieldErrors] = EmptyFields({
+	}: UserInterface): Promise<UserDocumentInterface> {
+		const user = await UserModel.create({
 			name,
 			email,
 			password,
+			avatarUrl: `https://avatars.dicebear.com/api/bottts/${name}.svg`,
 		})
 
-		if (hasEmptyFields) throw emptyFieldErrors
-		await ensureUniqueEmail(email!)
-
-		const hashedPassword = await hashPassword(password!)
-		avatarUrl ||= `https://avatars.dicebear.com/api/bottts/${name}.svg`
-
-		const user = await UserModel.create({
-			name,
-			email,
-			password: hashedPassword,
-			avatarUrl,
-		})
-
-		await user.save()
-
-		return user
+		return user.save()
 	}
 
-	static async signinWithEmailAndPassword({ email, password }: UserInterface) {
-		const [hasEmptyFields, emptyFieldErrors] = EmptyFields({ email, password })
-		if (hasEmptyFields) throw emptyFieldErrors
-
-		const user = await UserModel.findOne({ email })
-
-		if (!user) throw new Error('User not found')
-		if (!user.password) {
-			throw new Error(
-				'Email/Password combination associated with this user, please use social login'
-			)
-		}
-
-		const validPassword = await bcrypt.compare(password!, user.password!)
-		if (!validPassword) throw new Error('Invalid credentials')
-
-		return user
-	}
-
-	static async signInWithGoogle({
+	static async createFromGoogleProfile({
 		googleId,
 		name,
 		email,
-		imageUrl,
+		imageUrl: avatarUrl,
 	}: GoogleProfileInterface) {
-		const [hasEmptyFields, emptyFieldErrors] = EmptyFields({
-			googleId,
-			name,
-			email,
-			imageUrl,
-		})
-
-		if (hasEmptyFields) throw emptyFieldErrors
-
-		const account = await UserModel.findOne({
-			$or: [{ googleId }, { email }],
-		})
-
-		if (account != null) {
-			if (account.googleId) return account
-
-			account.googleId = googleId
-			await account.save()
-			return account
-		}
-
-		const user = await UserModel.create({
-			googleId,
-			name,
-			email,
-			avatarUrl: imageUrl,
-		})
-
-		await user.save()
-
-		return user
+		const user = await UserModel.create({ googleId, name, email, avatarUrl })
+		return user.save()
 	}
-}
 
-function hashPassword(password: string) {
-	return bcrypt.hash(password, 10)
-}
+	static async findByGoogleProfile({
+		googleId,
+		email,
+	}: GoogleProfileInterface): Promise<UserDocumentInterface> {
+		const user = await UserModel.findOne({ $or: [{ email }, { googleId }] })
+		return user as UserDocumentInterface
+	}
 
-async function ensureUniqueEmail(email: string) {
-	const emailIsInUse = await UserModel.exists({ email })
-	if (emailIsInUse) throw new Error('This e-mail is already in use')
+	static async associateAccountWithGoogle(
+		account: UserDocumentInterface,
+		{ googleId }: GoogleProfileInterface
+	): Promise<UserDocumentInterface> {
+		account.googleId = googleId
+		return account.save()
+	}
 }
 
 export default UserController
