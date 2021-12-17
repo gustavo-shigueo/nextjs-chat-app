842d4b0b16245247d54faa85fd76ea6658db753f
gustavo-shigueo <gustavo.gsmn@gmail.com>
2021-12-17 19:43:41 -0300

    Renaming and changing the creation of credentials

--- middlewares/giveCredentials.ts
+++ middlewares/giveCredentials.ts
@@ -1,15 +1,21 @@
-import InvalidOrExpiredTokenError from 'errors/InvalidOrExpiredTokenError'
-import NotAuthenticatedError from 'errors/NotAuthenticatedError'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 import AccessToken from 'tokens/AccessToken'
 import RefreshToken from 'tokens/RefreshToken'
 
-const giveCredentials = async (req: NextApiRequest, res: NextApiResponse) => {
+const giveCredentials = async (
+	req: NextApiRequest,
+	res: NextApiResponse,
+	id?: string
+) => {
+	if (id) req.body.user = { id }
 	const accessToken = AccessToken.create(req.body.user.id)
-	const refreshToken = RefreshToken.create(req.body.user.id)
+	const refreshToken = await RefreshToken.create(req.body.user.id)
 
 	res.setHeader('authorization', `Bearer: ${accessToken}`)
-	res.setHeader('cookie', `videochat-app-rt=${refreshToken}`)
+	setCookie({ res }, process.env.NEXT_PUBLIC_COOKIE_NAME!, refreshToken, {
+		maxAge: RefreshToken.maxAge,
+	})
 }
 
 export default giveCredentials
