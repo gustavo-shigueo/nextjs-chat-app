23d3732e33f668f95978aeeba7ec43d46c0a0dcf
gustavo-shigueo <gustavo.gsmn@gmail.com>
2022-07-27 16:29:00 -0300

    Deleted obsolete methods

--- services/Message/IMessageService.ts
+++ services/Message/IMessageService.ts
@@ -1,29 +1,5 @@
 import Message from 'entities/Message'
 
 export default interface IMessageService {
-	send(senderId: string, receiverId: string, text: string): Promise<Message>
-
-	listMessagesBetweenUsers(
-		userIdA: string,
-		userIdB: string,
-		cursor?: Date,
-		limit?: number
-	): Promise<Message[]>
-
-	latestMessageBetweenUsers(
-		userIdA: string,
-		userIdB: string
-	): Promise<Message | null>
-
-	listMessagesSentByUser(
-		userId: string,
-		cursor?: Date,
-		limit?: number
-	): Promise<Message[]>
-
-	listMessagesReceivedByUser(
-		userId: string,
-		cursor?: Date,
-		limit?: number
-	): Promise<Message[]>
+	send(senderId: string, chatId: string, text: string): Promise<Message>
 }
--- services/Message/MessageService.ts
+++ services/Message/MessageService.ts
@@ -9,53 +9,8 @@ export default class MessageService implements IMessageService {
 		this.#messageRepository = messageRepository
 	}
 
-	async send(
-		senderId: string,
-		receiverId: string,
-		text: string
-	): Promise<Message> {
-		const message = new Message(text, senderId, receiverId)
+	async send(senderId: string, chatId: string, text: string): Promise<Message> {
+		const message = new Message(text, senderId, chatId)
 		return this.#messageRepository.sendMessage(message)
-	}
-
-	async listMessagesBetweenUsers(
-		userIdA: string,
-		userIdB: string,
-		cursor?: Date | undefined,
-		limit?: number | undefined
-	): Promise<Message[]> {
-		return this.#messageRepository.listMessagesBetweenUsers(
-			userIdA,
-			userIdB,
-			cursor,
-			limit
-		)
-	}
-
-	async latestMessageBetweenUsers(
-		userIdA: string,
-		userIdB: string
-	): Promise<Message | null> {
-		return this.#messageRepository.latestMessageBetweenUsers(userIdA, userIdB)
-	}
-
-	async listMessagesSentByUser(
-		userId: string,
-		cursor?: Date | undefined,
-		limit?: number | undefined
-	): Promise<Message[]> {
-		return this.#messageRepository.listMessagesSentByUser(userId, cursor, limit)
-	}
-
-	async listMessagesReceivedByUser(
-		userId: string,
-		cursor?: Date | undefined,
-		limit?: number | undefined
-	): Promise<Message[]> {
-		return this.#messageRepository.listMessagesReceivedByUser(
-			userId,
-			cursor,
-			limit
-		)
 	}
 }
