3946b74a49d2b129cafe1380c8b2335180df3829
gustavo-shigueo <gustavo.gsmn@gmail.com>
2023-10-26 08:35:24 -0300

    Add warnings about empty contact list

--- src/components/ChatsPanel/NewChatDialog/NewGroupChatPanel/GroupMemberList/GroupMemberList.tsx
+++ src/components/ChatsPanel/NewChatDialog/NewGroupChatPanel/GroupMemberList/GroupMemberList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import List from '../../../../../components/List'
 import { useTabs } from '../../../../../components/Tabs/Tabs'
 import { api } from '../../../../../utils/api'
@@ -17,6 +17,13 @@ export default function GroupMemberList({
 }: GroupMemberListProps) {
 	const { selectedIndex } = useTabs()
 	const { data, refetch } = api.contacts.list.useQuery()
+	const filtered = useMemo(() => {
+		if (!data) return null
+
+		return data.filter(x =>
+			x.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+		)
+	}, [data, search])
 
 	useEffect(() => {
 		if (selectedIndex === 1) {
@@ -24,15 +31,29 @@ export default function GroupMemberList({
 		}
 	}, [selectedIndex, refetch])
 
-	if (!data) return <></>
+	if (!data || !filtered) return <></>
+
+	if (data.length === 0) {
+		return (
+			<strong className="text-center">
+				Sua lista de contatos está vazia. Adicione contatos para criar um grupo
+			</strong>
+		)
+	}
+
+	if (filtered.length === 0) {
+		return (
+			<strong className="text-center">
+				Nenhum contato corresponde à pesquisa
+			</strong>
+		)
+	}
 
 	return (
 		<List
 			className="divide-y-2 divide-neutral-800 dark:divide-neutral-50"
 			itemKey="id"
-			items={data.filter(x =>
-				x.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-			)}
+			items={filtered}
 			renderItem={contact => (
 				<GroupMemberListItem
 					checked={selectedUsers.includes(contact.id)}
--- src/components/ContactList/ContactList.tsx
+++ src/components/ContactList/ContactList.tsx
@@ -3,6 +3,7 @@ import List from '../List'
 import { api } from '../../utils/api'
 import ContactListItem from './ContactListItem'
 import type { UserSchema } from '../../server/api/schemas/userSchema'
+import { useMemo } from 'react'
 
 type ContactListProps = {
 	search: string
@@ -16,8 +17,52 @@ export default function ContactList({
 	filter,
 }: ContactListProps) {
 	const { data } = api.contacts.list.useQuery()
+	const filtered = useMemo(() => {
+		if (!data) return null
 
-	if (!data) return <></>
+		return data.filter(x => {
+			const searchResult = x.name
+				.toLocaleLowerCase()
+				.includes(search.toLocaleLowerCase())
+
+			if (!filter) return searchResult
+
+			return searchResult && filter(x)
+		})
+	}, [data, search, filter])
+
+	if (!data || !filtered) return <></>
+
+	if (data.length === 0) {
+		return (
+			<Form
+				onSubmit={e => {
+					e.preventDefault()
+					e.currentTarget.closest('dialog')?.close()
+				}}
+			>
+				<strong className="text-center">
+					Sua lista de contatos está vazia. Adicione um contato para iniciar uma
+					conversa
+				</strong>
+			</Form>
+		)
+	}
+
+	if (filtered.length === 0) {
+		return (
+			<Form
+				onSubmit={e => {
+					e.preventDefault()
+					e.currentTarget.closest('dialog')?.close()
+				}}
+			>
+				<strong className="text-center">
+					Nenhum contato corresponde à pesquisa
+				</strong>
+			</Form>
+		)
+	}
 
 	return (
 		<Form
@@ -29,15 +74,7 @@ export default function ContactList({
 			<List
 				className="divide-y-2 divide-neutral-800 dark:divide-neutral-50"
 				itemKey="id"
-				items={data.filter(x => {
-					const searchResult = x.name
-						.toLocaleLowerCase()
-						.includes(search.toLocaleLowerCase())
-
-					if (!filter) return searchResult
-
-					return searchResult && filter(x)
-				})}
+				items={filtered}
 				renderItem={contact => (
 					<ContactListItem contact={contact} onClick={onContactSelected} />
 				)}
