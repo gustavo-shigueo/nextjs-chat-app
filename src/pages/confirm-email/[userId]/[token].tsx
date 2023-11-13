e73539cb06f30addff019567a530bf4579e3f464
gustavo-shigueo <gustavo.gsmn@gmail.com>
2023-11-13 07:51:11 -0300

    Add basic styling to confirm email pages

--- src/pages/confirm-email/[userId]/[token].tsx
+++ src/pages/confirm-email/[userId]/[token].tsx
@@ -3,6 +3,10 @@ import { api } from '../../../utils/api'
 import { useEffect, useState } from 'react'
 import Spinner from '../../../components/Spinner'
 import Link from 'next/link'
+import {
+	IoMdCloseCircleOutline,
+	IoMdCheckmarkCircleOutline,
+} from 'react-icons/io'
 
 export default function ConfirmEmail() {
 	const [status, setStatus] = useState<
@@ -33,29 +37,56 @@ export default function ConfirmEmail() {
 	switch (status) {
 		case 'loading':
 			return (
-				<main>
+				<main className="grid place-items-center gap-2">
 					<Spinner />
 				</main>
 			)
 
 		case 'error':
 			return (
-				<main>
-					Algo deu errado! Clique{' '}
-					<Link
-						href={`/${locale ?? defaultLocale}/confirmEmail/${
-							userId as string
-						}`}
-					>
-						aqui
-					</Link>{' '}
-					para solicitar o reenvio do e-mail de confirmação
+				<main className="grid place-items-center gap-2">
+					<section>
+						<h2 className="text-center text-2xl font-bold">Algo deu errado!</h2>
+
+						<span>
+							<IoMdCloseCircleOutline
+								className="text-red-500 mli-auto"
+								size={150}
+							/>
+						</span>
+
+						<p className="text-neutral-700 dark:text-neutral-300">
+							Clique{' '}
+							<Link
+								className="m-0 cursor-pointer appearance-none border-none bg-transparent p-0 font-bold text-neutral-800 underline dark:text-neutral-200"
+								href={`/${locale ?? defaultLocale}/confirmEmail/${
+									userId as string
+								}`}
+							>
+								aqui
+							</Link>{' '}
+							para solicitar o reenvio do e-mail de confirmação
+						</p>
+					</section>
 				</main>
 			)
 
 		case 'success':
-			return <main className='grid place-items-center gap-2'>
-        E-mail confirmado com sucesso!
-      </main>
+			return (
+				<main className="grid place-items-center gap-2">
+					<section>
+						<h2 className="text-center text-2xl font-bold">
+							E-mail confirmado com sucesso!
+						</h2>
+
+						<span>
+							<IoMdCheckmarkCircleOutline
+								className="text-green-500 mli-auto"
+								size={150}
+							/>
+						</span>
+					</section>
+				</main>
+			)
 	}
 }
--- src/pages/confirm-email/[userId]/index.tsx
+++ src/pages/confirm-email/[userId]/index.tsx
@@ -1,8 +1,8 @@
 import { api } from '../../../utils/api'
-import Button from '../../../components/Button'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Spinner from '../../../components/Spinner'
+import { IoMail } from 'react-icons/io5'
 
 export default function ConfirmationMessage() {
 	const requestConfirmation = api.users.requestConfirmationEmail.useMutation()
@@ -39,8 +39,8 @@ export default function ConfirmationMessage() {
 
 	if (isLoading) {
 		return (
-			<main>
-				<Spinner />
+			<main className="grid place-items-center">
+				<Spinner size={5} />
 			</main>
 		)
 	}
@@ -50,23 +50,37 @@ export default function ConfirmationMessage() {
 	}
 
 	return (
-		<main className="grid gap-2 justify-center">
-			<h2>Seu cadastro foi realizado com sucesso!</h2>
-			<p>Confirme seu email para fazer login</p>
+		<main className="grid place-items-center gap-2">
+			<section>
+				<h2 className="text-center text-2xl font-bold">
+					Seu cadastro foi realizado com sucesso!
+				</h2>
 
-			<p>
-				Não recebeu o e-mail de confirmação?{' '}
-				<Button
-					variant="primary"
-					disabled={timer > 0}
-					onClick={() => {
-						requestConfirmation.mutate({ email: data.email })
-						setTimer(30)
-					}}
-				>
-					Clique aqui{timer > 0 && ` em ${timer} segundos`}
-				</Button>
-			</p>
+				<span>
+					<IoMail className="mli-auto" size={150} />
+				</span>
+
+				<p>Confirme seu email para fazer login</p>
+
+				<p className="text-neutral-700 dark:text-neutral-300">
+					Não recebeu o e-mail de confirmação?{' '}
+					{timer > 0 ? (
+						<span className="text-neutral-600 dark:text-neutral-400">
+							Por favor aguarde {timer} segundos
+						</span>
+					) : (
+						<button
+							className="m-0 cursor-pointer appearance-none border-none bg-transparent p-0 font-bold text-neutral-800 underline dark:text-neutral-200"
+							onClick={() => {
+								requestConfirmation.mutate({ email: data.email })
+								setTimer(30)
+							}}
+						>
+							Clique aqui
+						</button>
+					)}
+				</p>
+			</section>
 		</main>
 	)
 }
